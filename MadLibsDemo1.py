b3d37d42a18b83bbc7db5159d0156a8f5af1dc5e
Tyler Owen <32280382+Manaman1000@users.noreply.github.com>
2019-07-08 16:21:58 -0400

    Create MadLibsDemo1.py

---
+++ MadLibsDemo1.py
@@ -0,0 +1,30 @@
+#This is a practice Mad Libs demo for myself.
+
+
+celeb1 = input("Enter a celebrity: )"
+adj1 = input("Enter an adjective: ")
+direc1 = input("Enter a direction: ")
+color1 = input("Enter a color: ")
+pl_noun1 = input("Enter a plural noun: ")
+adj2 = input("Enter an adjective: ")
+pronoun1 = input("Enter a pronoun: ")
+pst_action1 = input("Enter a past tense action: ")
+obj1 = input("Enter an object: ")
+celeb2 = input("Enter a celebrity: ")
+prop_noun1 = input("Enter a proper noun: ")
+
+
+
+print(celeb1 + " awoke with a " + adj1 + ".")
+print("They looked " + direc1 + " at their " + color1 +" "+ pl_noun1 +" with " + adj2 + ".")
+print("Suddenly, " + pronoun1 + " made sense! " + celeb1 +" "+ pst_action1 + " to their bedroom " + obj1 + " only to find that " 
++ celeb2 + " was the true " + prop_noun1 + " all along!")
+
+
+
+
+
+
+
+
+
