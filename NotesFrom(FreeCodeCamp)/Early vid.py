cf1f527f6c4be009bdbddb044ddae08531bf98ac
Tyler Owen <32280382+Manaman1000@users.noreply.github.com>
2019-07-15 17:13:26 -0400

    Create Early vid.py

---
+++ NotesFrom(FreeCodeCamp)/Early vid.py
@@ -0,0 +1,22 @@
+# vbasic stuff from begin of video tutorial
+
+print("   /|")
+print("  / |")
+print(" /  |")
+print("/___|\n\n")
+
+
+# Using strings and variables
+character_name = "Bill"
+character_age = "14"
+print("There once was a man named " + character_name +",")
+print("He was " + character_age + " years old.\n")
+print("He really liked the name " + character_name + ",")
+print("However, he did not like being " + character_age +".\n\n")
+
+# Creating and using functions
+phrase = "Giraffe Academy"
+print(phrase.upper())
+print(phrase.isupper())
+print(phrase.upper().isupper())
+print(len(phrase))
